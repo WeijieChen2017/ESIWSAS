5bf95fa882d74d8efeefb7a9fec7adff528a1daa
WeijieChen2017 <weijiechen1994@gmail.com>
2024-02-28 16:09:26 -0600

    Good morning!

--- compute_metrics.py
+++ compute_metrics.py
@@ -5,22 +5,34 @@ proj_list = [
     ["Seg532_Unet_neuron_dropoutRate_010"],
 ]
 
+img_files = [
+    "img0026",
+    "img0027",
+    "img0028",
+    "img0029",
+    "img0030",
+    "img0031",
+]
+
 import numpy as np
 
-for idx, proj_info in enumerate(proj_list):
+for idx_proj, proj_info in enumerate(proj_list):
     
-    print(idx+1, proj_info[0])
+    print(idx_proj+1, proj_info[0])
 
     # load from img0026 to img0031 ending 
     # _y_RAS_1.5_1.5_2.0_vote.npy and _z_RAS_1.5_1.5_2.0_vote.npy
     # then compute the dice score
 
-    path_y = "./project_dir/" + proj_list[idx][0] + "_y_RAS_1.5_1.5_2.0_vote.npy"
-    path_z = "./project_dir/" + proj_list[idx][0] + "_z_RAS_1.5_1.5_2.0_vote.npy"
+    for idx_img, img_name in enumerate(img_files):
+        print(idx_img+1, img_name)
 
-    data_y = np.load(path_y, allow_pickle=True)
-    data_z = np.load(path_z, allow_pickle=True)
+        path_y = "./results/moved_files_Nov5/" + proj_info[0] + "_" + img_name + "_y_RAS_1.5_1.5_2.0_vote.npy"
+        path_z = "./results/moved_files_Nov5/" + proj_info[0] + "_" + img_name + "_z_RAS_1.5_1.5_2.0_vote.npy"
 
-    print(data_y.shape, data_z.shape)
+        data_y = np.load(path_y, allow_pickle=True)
+        data_z = np.load(path_z, allow_pickle=True)
+
+        print(img_name, data_y.shape, data_z.shape)
 
     print()
